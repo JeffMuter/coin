3a429628106e092ea7c25fcd38529cf3d33fe090
JeffMuter <muterjeffery@gmail.com>
2024-09-17 09:54:55 -0400

    update addresses to listen from

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	fmt.Println("Starting Chatroom Server...")
 
 	srv := server.NewRoomServer()
-	err := srv.Start("localhost:8080")
+	err := srv.Start("0.0.0.0:8080")
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 		os.Exit(1)
